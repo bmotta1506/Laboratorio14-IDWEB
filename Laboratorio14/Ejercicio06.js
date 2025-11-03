019bc8e626f7162411f81813a752d3acfd406660
bmotta1506 <bmotta1506@gmail.com>
2025-11-03 16:53:48 -0500

    Ejercicio 06

---
+++ Laboratorio14/Ejercicio06.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("Ingrese un numero"))
+
+const cuadrado = Math.pow(numero,2);
+const cubo =  Math.pow(numero,3);
+const raiz =  Math.sqrt(numero);
+
+
+
+console.log("Operaciones con el numero: " + numero);
+console.log(`Cuadrado: ${cuadrado}`)
+console.log(`Cubo: ${cubo}`)
+console.log(`Raiz: ${raiz}`)
