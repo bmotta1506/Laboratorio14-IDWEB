f043764ad552fb9acd16bcf9d882af32c314dd20
bmotta1506 <bmotta1506@gmail.com>
2025-11-05 23:22:04 -0500

    Ejercicio 13

---
+++ Laboratorio14/Ejercicio13.js
@@ -0,0 +1,51 @@
+class Figura {
+    constructor() {}
+
+    area() {
+        return 0;
+    }
+
+    perimetro() {
+        return 0;
+    }
+}
+
+class Cuadrado extends Figura {
+    constructor(lado) {
+        super();
+        this.lado = lado;
+    }
+
+    area() {
+        return this.lado * this.lado;
+    }
+
+    perimetro() {
+        return this.lado * 4;
+    }
+}
+
+class Triangulo extends Figura {
+    constructor(base, altura, lado1, lado2, lado3) {
+        super();
+        this.base = base;
+        this.altura = altura;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    area() {
+        return (this.base * this.altura) / 2;
+    }
+
+    perimetro() {
+        return this.lado1 + this.lado2 + this.lado3;
+    }
+}
+
+let c = new Cuadrado(5);
+console.log(`Área del cuadrado: ${c.area()}, Perímetro del cuadrado: ${c.perimetro()}`);
+
+let t = new Triangulo(4, 3, 4, 3, 5);
+console.log(`Área del triángulo: ${t.area()}, Perímetro del triángulo: ${t.perimetro()}`);
